bc8c235cb1e8615a15050a450e80fa1a3e787015
Sebastien Rousseau <sebounoob@gmail.com>
2021-08-13 11:59:27 +0200

    first push"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'movies#index'
+  root to: 'lists#index'
   resources :lists, only: [:new, :create, :index, :show ] do
     resources :bookmarks, only: [ :new, :create ]
   end
